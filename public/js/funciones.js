ce6bc8cad7ca73a7dd97408e0c3eab58a3f0e601
Hector Rendon <hrchilpo@gmail.com>
2021-08-22 16:34:11 -0500

    Se agregó la función ACTUALIZAR

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,18 +1,51 @@
 const btnEliminar = document.querySelector('#btnEliminar')
-btnEliminar.addEventListener('click', async ()=>{
-    console.log('Me picaste!')
+btnEliminar.addEventListener('click', async () => {
     const id = btnEliminar.dataset.id
-    console.log('id: ',id)
+    console.log('id: ', id)
     try {
-        const data = await fetch(`/mascotas/${id}`, {method: 'delete'})
+        const data = await fetch(`/mascotas/${id}`, {
+            method: 'delete'
+        })
         const res = await data.json()
 
         if (res.estado) {
             window.location.href = '/mascotas'
         } else {
             console.log(res.mensaje)
-        }  
+        }
     } catch (error) {
         console.log(error)
+    }
+})
+
+
+const formEditar = document.querySelector('#formEditar')
+formEditar.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    //Obtener la informacion de los inputs para enviarlas
+    const nombre = document.querySelector('#nombreInput').value
+    const descripcion = document.querySelector('#descripcionInput').value
+    const id = formEditar.dataset.id
+    console.log(nombre,descripcion,id)
+    try {
+        const data = await fetch(`/mascotas/${id}`, {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nombre,
+                descripcion
+            })
+        })
+
+        const res = await data.json();
+        console.log(res.mensaje)
+        window.location.href = '/mascotas'
+
+    } catch (error) {
+        console.log(error)
+        console.log(res.mensaje)
     }
 })
--- router/mascotas.js
+++ router/mascotas.js
@@ -25,11 +25,6 @@ router.get('/crear', (req, res)=>{
 router.post('/', async (req, res)=>{
     const body = req.body;
     try {
-        // Agregar info a la base de datos Metodo 1
-        //const mascotaDB = new Mascota(body)
-        //await mascotaDB.save()
-
-        //Agregar info a la BD Metodo 2
         await Mascota.create(body)
 
         res.redirect('/mascotas')
@@ -43,14 +38,14 @@ router.get('/:id', async (req, res)=>{
     try {
         const mascotaDB = await Mascota.findOne({ _id: id })
         res.render('detalle', {
-            titulo: 'Eliminar datos',
+            titulo: 'Actualizar datos',
             mascota: mascotaDB,
             error: false,
         })
     } catch (error) {
         console.log(error)
         res.render('detalle', {
-            titulo: 'Eliminar datos',
+            titulo: 'Actualizar datos',
             error: true,
             mensaje: 'No se encontro el ID seleccionado'
         })
@@ -74,6 +69,29 @@ router.delete('/:id', async(req, res)=>{
         }
     } catch (error) {
         console.log(error)
+    }
+})
+
+router.put('/:id', async(req, res)=>{
+    const id = req.params.id
+    const body = req.body
+
+    console.log(id)
+    console.log(body)
+    try {
+        const mascotaDB = await Mascota.findByIdAndUpdate(id, body, { useFindAndModify: false })
+        console.log(mascotaDB)
+
+        res.json({
+            estado: true,
+            mensaje: 'Información Actualizada correctamente!'
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            estado: false,
+            mensaje: 'Hubo un error al editar!'
+        })
     }
 })
 
